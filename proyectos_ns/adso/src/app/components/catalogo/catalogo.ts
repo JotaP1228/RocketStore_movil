1d79c955041c601ee57f65ef4025dcc922eddefa
sebastianEspino <sebasespinosa2016@gmail.com>
2024-02-21 11:28:25 -0500

    confirmacion catalogo

--- proyectos_ns/adso/src/app/components/catalogo/catalogo.ts
+++ proyectos_ns/adso/src/app/components/catalogo/catalogo.ts
@@ -8,22 +8,22 @@ import { SegmentedBar, SegmentedBarItem, SelectedIndexChangedEventData } from "@
 })
 export class CatalogoComponent {
   mySegmentedBarItems: Array<SegmentedBarItem> = [];
-  public constructor(private router: Router) {
-    for (let i = 1; i = 1; i++) {
-      const item = new SegmentedBarItem();
-      item.title = "Hombre", "Mujer", "Niño", "Niña" + i;
-      this.mySegmentedBarItems.push(item);
-  } 
+  items = ['Hombre', 'Mujer', 'Niño', 'Niña']
 
-// Use the component constructor to inject providers.
-  }
+    constructor(private router: Router) {
+        for (let i = 0; i < 4; i++) {
+            const item = new SegmentedBarItem();
+            item.title = this.items[i];
+            this.mySegmentedBarItems.push(item);
+        }
+    }
+    
 
-  public onSelectedIndexChange(args: SelectedIndexChangedEventData) {
-    const segmentedBar = args.object as SegmentedBar;
-    const oldIndex = args.oldIndex;
-    const newIndex = args.newIndex;
-  
-  }
+    public onSelectedIndexChange(args: SelectedIndexChangedEventData) {
+        const segmentedBar = args.object as SegmentedBar;
+        const oldIndex = args.oldIndex;
+        const newIndex = args.newIndex;
+    }
 
   public onTap(){
     this.router.navigate(["home"]);
